344f79684c011b40e838a80de16c40d789749688
Rata0 <nekon689@gmail.com>
2025-07-18 20:18:04 +0300

    Add CustomError class with HTTP status methods

---
+++ server/src/error/СustomError.ts
@@ -0,0 +1,21 @@
+class CustomError extends Error {
+  constructor(public status: number, public message: string) {
+    super(message)
+    this.status = status
+    this.message = message
+  }
+
+  static badRequest(message: string) {
+    return new CustomError(400, message)
+  }
+
+  static unauthorized(message: string) {
+    return new CustomError(401, message)
+  }
+
+  static forbidden(message: string) {
+    return new CustomError(403, message)
+  }
+}
+
+export default CustomError
