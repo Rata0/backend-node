65fff5ee871476ff57879a287f299e5bc37590c9
Rata0 <nekon689@gmail.com>
2025-07-18 20:20:04 +0300

    Formatting fields

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -2,19 +2,36 @@ import sequelize from '../db'
 import { DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize'
 
 interface UserModel extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> {
-  id: number
+  id?: number
   name: string
   email: string
   password: string
-  role: string
+  role: string,
 }
 
 const User = sequelize.define<UserModel>('user', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING },
-  email: { type: DataTypes.STRING, unique: true },
-  password: { type: DataTypes.STRING },
-  role: { type: DataTypes.STRING, defaultValue: 'USER' },
+  id: { 
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true
+  },
+  name: { 
+    type: DataTypes.STRING,
+    allowNull: false 
+  },
+  email: { 
+    type: DataTypes.STRING,
+    allowNull: false, 
+    unique: true 
+  },
+  password: { 
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: { 
+    type: DataTypes.STRING, 
+    defaultValue: 'USER' 
+  },
 })
 
 export default User
